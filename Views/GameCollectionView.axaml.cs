6de456b171994d85d4d528cbb4b64ac1aecd575c
Michał Potocki <s61451@student.ubb.edu.pl>
2025-04-24 19:32:59 +0200

    "Widok Kolekcji Gier (Placeholder)".

---
+++ Views/GameCollectionView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace OmniMedia.Views;
+
+public partial class GameCollectionView : UserControl
+{
+    public GameCollectionView()
+    {
+        InitializeComponent();
+    }
+}
