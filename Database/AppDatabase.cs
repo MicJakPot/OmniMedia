5dab5328555d5e7d6398b742d8229c8eda82f241
Michał Potocki <s61451@student.ubb.edu.pl>
2025-04-24 21:53:23 +0200

    Usuwanie działa ale nie usuwa (id gry=0)

--- Database/AppDatabase.cs
+++ Database/AppDatabase.cs
@@ -1,61 +1,84 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Threading.Tasks;
-using OmniMedia.Models; 
-using SQLite; 
+using SQLite; // Potrzebne do SQLiteAsyncConnection i atrybutów
+using OmniMedia.Models; // Potrzebne do modelu Game
+using System.Diagnostics; // Potrzebne do Debug.WriteLine
 
 namespace OmniMedia.Database
 {
-    // Klasa obsługująca połączenie z bazą danych SQLite
-    public class AppDatabase : SQLiteAsyncConnection // Dziedziczymy z SQLiteAsyncConnection dla operacji asynchronicznych
+    // Klasa obsługująca połączenie z bazą danych SQLite i operacje CRUD na danych
+    public class AppDatabase
     {
-        // Konstruktor klasy AppDatabase
-        public AppDatabase(string dbPath) : base(dbPath)
+        // Asynchroniczne połączenie z bazą danych SQLite
+        private readonly SQLiteAsyncConnection _database;
+
+        // Konstruktor - inicjalizuje połączenie z bazą danych i tworzy tabele
+        public AppDatabase(string dbPath)
         {
-            // Konstruktor klasy bazowej (SQLiteAsyncConnection) inicjalizuje połączenie
+            _database = new SQLiteAsyncConnection(dbPath);
+            // Tworzenie tabeli Game (i innych, jeśli będą potrzebne)
+            // Ważne: tworzenie tabel powinno odbywać się asynchronicznie
+            // Najlepiej wywołać CreateTablesAsync zaraz po utworzeniu instancji AppDatabase
+            // Task.Run(() => CreateTablesAsync()).Wait(); // Można też wywołać gdzieś na początku aplikacji
         }
 
-        // Metoda inicjalizująca strukturę bazy danych (tworzy tabele)
-        public async Task CreateTablesAsync()
+        // Metoda tworząca tabele w bazie danych
+        public Task CreateTablesAsync()
         {
-            // Tworzy tabelę dla modelu Game, jeśli jeszcze nie istnieje
-            await CreateTableAsync<Game>();
-            // Można dodać tworzenie tabel dla innych modeli (Music, Movie) w przyszłości
+            Debug.WriteLine("[AppDatabase] Tworzenie tabel bazy danych...");
+            // Tworzy tabelę Game, jeśli jeszcze nie istnieje
+            return _database.CreateTableAsync<Game>();
+            // Można dodać tworzenie innych tabel tutaj
+            // return _database.CreateTablesAsync<Game, Music, Movie>(); // Przykład dla wielu tabel
         }
 
-        // Metoda do zapisywania (lub aktualizowania) obiektu Game w bazie
+        // Metoda pobierająca wszystkie gry z bazy danych
+        public Task<List<Game>> GetGamesAsync()
+        {
+            Debug.WriteLine("[AppDatabase] Pobieranie wszystkich gier z bazy danych...");
+            // Zwraca listę wszystkich obiektów Game z tabeli
+            return _database.Table<Game>().ToListAsync();
+        }
+
+        // Metoda pobierająca konkretną grę po ID
+        public Task<Game?> GetGameAsync(int id)
+        {
+            Debug.WriteLine($"[AppDatabase] Pobieranie gry o ID: {id} z bazy danych...");
+            // Zwraca grę z konkretnym ID, lub null jeśli nie znaleziono
+            return _database.Table<Game>().Where(i => i.Id == id).FirstOrDefaultAsync();
+        }
+
+        // Metoda zapisująca (wstawiająca lub aktualizująca) grę w bazie danych
         public Task<int> SaveGameAsync(Game game)
         {
+            Debug.WriteLine($"[AppDatabase] Zapisywanie gry '{game.Title}' do bazy danych...");
             if (game.Id != 0)
             {
-                // Obiekt już istnieje (ma Id), więc aktualizujemy
-                return UpdateAsync(game);
+                // Jeśli Id jest różne od 0, zakładamy, że rekord już istnieje i go aktualizujemy
+                Debug.WriteLine($"[AppDatabase] Aktualizacja gry o ID: {game.Id}");
+                return _database.UpdateAsync(game);
             }
             else
             {
-                // Obiekt jest nowy (Id == 0), więc wstawiamy
-                return InsertAsync(game);
+                // Jeśli Id jest 0, zakładamy, że to nowy rekord i go wstawiamy
+                Debug.WriteLine($"[AppDatabase] Wstawianie nowej gry: {game.Title}");
+                return _database.InsertAsync(game);
             }
         }
 
-        // Metoda do pobierania wszystkich gier z bazy
-        public Task<List<Game>> GetGamesAsync()
-        {
-            // Pobiera wszystkie wiersze z tabeli Games
-            return Table<Game>().ToListAsync();
-        }
-
-        // Metoda do pobierania pojedynczej gry po Id (opcjonalnie)
-        public Task<Game> GetGameAsync(int id)
-        {
-            return Table<Game>().Where(i => i.Id == id).FirstOrDefaultAsync();
-        }
-
-        // Metoda do usuwania gry (opcjonalnie)
+        // NOWA METODA: Metoda usuwająca grę z bazy danych
         public Task<int> DeleteGameAsync(Game game)
         {
-            return DeleteAsync(game);
+            Debug.WriteLine($"[AppDatabase] Usuwanie gry '{game.Title}' o ID: {game.Id} z bazy danych...");
+            // Usuwa rekord odpowiadający przekazanemu obiektowi Game
+            return _database.DeleteAsync(game);
         }
+
+        // TODO: Można dodać metody dla innych operacji i typów danych (muzyka, filmy)
+        // public Task<List<Music>> GetMusicAsync() { ... }
+        // public Task<int> SaveMusicAsync(Music music) { ... }
+        // public Task<int> DeleteMusicAsync(Music music) { ... }
+        // Analogicznie dla filmów
     }
 }
--- ViewModels/GameCollectionViewModel.cs
+++ ViewModels/GameCollectionViewModel.cs
@@ -4,10 +4,13 @@ using OmniMedia.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
-using System.Threading.Tasks;
-using OmniMedia.Database;
-using Avalonia.Threading;
-using System.Diagnostics;
+using System.Threading.Tasks; // Potrzebne do Task
+using Avalonia.Threading; // Potrzebne do Dispatcher.UIThread
+using System.Diagnostics; // Potrzebne do Debug.WriteLine
+using OmniMedia.Database; // Potrzebne do AppDatabase
+using System.Reactive; // Potrzebne do Unit
+using System.Reactive.Linq; // Potrzebne do Select
+using System.Linq; // Potrzebne do FirstOrDefault
 
 namespace OmniMedia.ViewModels
 {
@@ -15,36 +18,132 @@ namespace OmniMedia.ViewModels
     public class GameCollectionViewModel : ViewModelBase
     {
         // Właściwość na kolekcję gier do wyświetlenia
-        public ObservableCollection<Game> Games { get; set; }
+        private ObservableCollection<Game> _games = new ObservableCollection<Game>();
+        public ObservableCollection<Game> Games
+        {
+            get => _games;
+            set => this.RaiseAndSetIfChanged(ref _games, value);
+        }
+
+        // NOWA WŁAŚCIWOŚĆ: Właściwość na zaznaczoną grę w kolekcji
+        // Typem jest Game, bo to obiekty Game są w kolekcji Games
+        private Game? _selectedGame;
+        public Game? SelectedGame
+        {
+            get => _selectedGame;
+            set => this.RaiseAndSetIfChanged(ref _selectedGame, value);
+        }
+
+        // NOWA KOMENDA: Komenda do usuwania zaznaczonej gry z kolekcji
+        public ReactiveCommand<Unit, Unit> RemoveGameCommand { get; }
+
+
+        // TODO: Właściwość lub kolekcja dla miniatur ostatnio dodanych treści (jeśli są w tym ViewModelu)
+        // public ObservableCollection<RecentlyAddedItemViewModel> RecentlyAddedItems { get; }
+
 
         // Konstruktor ViewModelu
         public GameCollectionViewModel()
         {
-            Games = new ObservableCollection<Game>(); // Inicjalizacja kolekcji
+            // Inicjalizacja komendy usuwania
+            // Komenda będzie aktywna tylko, gdy SelectedGame nie jest null
+            RemoveGameCommand = ReactiveCommand.CreateFromTask(PerformRemoveGameAsync,
+                                                               this.WhenAnyValue(x => x.SelectedGame)
+                                                                    .Select(selectedGame => selectedGame != null));
+
 
             // Wczytaj gry z bazy danych po utworzeniu ViewModelu
             // Uruchamiamy ładowanie asynchronicznie, aby nie blokować UI
             Task.Run(LoadGamesAsync);
+
+            // TODO: Logika ładowania miniatur ostatnio dodanych treści (jeśli są w tym ViewModelu)
         }
 
         // Metoda asynchroniczna do ładowania gier z bazy danych
         private async Task LoadGamesAsync()
         {
-            // Próba pobrania z bazy danych
-            var dummyGames = await App.Database.GetGamesAsync();
-
-            // Upewniamy się, że aktualizacja ObservableCollection odbywa się w wątku UI
-            await Dispatcher.UIThread.InvokeAsync(() =>
+            try
             {
-                Games.Clear();
-                foreach (var game in dummyGames)
+                Debug.WriteLine("[GameCollectionViewModel] Rozpoczynam ładowanie gier z bazy danych.");
+                var gamesFromDb = await App.Database.GetGamesAsync(); // Pobierz gry z bazy
+
+                // Upewniamy się, że aktualizacja ObservableCollection odbywa się w wątku UI
+                await Dispatcher.UIThread.InvokeAsync(() =>
                 {
-                    Games.Add(game);
-                }
-                Debug.WriteLine($"[GameCollectionViewModel] Załadowano {Games.Count} gier z bazy danych.");
-            });
+                    Games.Clear(); // Wyczyść obecną kolekcję
+                    foreach (var game in gamesFromDb)
+                    {
+                        Games.Add(game); // Dodaj gry pobrane z bazy
+                    }
+                    Debug.WriteLine($"[GameCollectionViewModel] Załadowano {Games.Count} gier z bazy danych.");
+                });
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"[GameCollectionViewModel] Błąd podczas ładowania gier z bazy danych: {ex.Message}");
+                // TODO: Poinformuj użytkownika o błędzie
+            }
         }
 
-        // Można dodać inne właściwości lub komendy, np. do sortowania, filtrowania, usuwania gier
+
+        // Metoda asynchroniczna do usuwania zaznaczonej gry z bazy danych
+        private async Task PerformRemoveGameAsync()
+        {
+            // Sprawdzamy, czy jakaś gra jest zaznaczona
+            if (SelectedGame != null)
+            {
+                Debug.WriteLine($"[GameCollectionViewModel] Próba usunięcia gry z kolekcji: {SelectedGame.Title}");
+
+                try
+                {
+                    // WYWOŁAJ METODĘ USUWANIA Z BAZY DANYCH
+                    // Używamy metody DeleteGameAsync, którą dodaliśmy w AppDatabase.cs
+                    int result = await App.Database.DeleteGameAsync(SelectedGame);
+
+                    Debug.WriteLine($"[GameCollectionViewModel] Wynik usuwania gry z bazy: {result}");
+
+                    // Jeśli usuwanie z bazy się powiodło (result > 0), usuń grę z ObservableCollection Games
+                    if (result > 0)
+                    {
+                        await Dispatcher.UIThread.InvokeAsync(() =>
+                        {
+                            // Znajdujemy i usuwamy grę z kolekcji Games
+                            // Używamy Id do znalezienia gry, bo SelectedGame może być innym obiektem niż ten w kolekcji, ale ma to samo Id
+                            var gameToRemove = Games.FirstOrDefault(g => g.Id == SelectedGame.Id);
+                            if (gameToRemove != null)
+                            {
+                                Games.Remove(gameToRemove); // Usuń grę z listy wyświetlanej w UI
+                                Debug.WriteLine($"[GameCollectionViewModel] Usunięto grę '{gameToRemove.Title}' z listy w UI.");
+                            }
+                            SelectedGame = null; // Odznaczamy element po usunięciu
+                        });
+
+                        // TODO: Poinformuj użytkownika o sukcesie usunięcia (np. wyświetl komunikat w UI)
+                        Debug.WriteLine($"[GameCollectionViewModel] Gra '{SelectedGame.Title}' pomyślnie usunięta.");
+
+                    }
+                    else
+                    {
+                        Debug.WriteLine($"[GameCollectionViewModel] Nie udało się usunąć gry '{SelectedGame.Title}' z bazy danych (result był 0).");
+                        // TODO: Poinformuj użytkownika o błędzie/braku usunięcia
+                    }
+
+
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine($"[GameCollectionViewModel] Błąd podczas usuwania gry z bazy: {ex.Message}");
+                    // TODO: Poinformuj użytkownika o błędzie
+                }
+            }
+            else
+            {
+                Debug.WriteLine("[GameCollectionViewModel] Próba usunięcia z kolekcji, ale żadna gra nie jest zaznaczona.");
+            }
+        }
+
+        // TODO: Możesz potrzebować ViewModelu dla pojedynczej miniaturki (jeśli miniatury ostatnio dodanych są w tym ViewModelu)
+        // public class RecentlyAddedItemViewModel : ViewModelBase { ... }
+
     }
 }
