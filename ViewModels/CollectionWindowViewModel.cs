c07600a56f2cb31d664f5135b7e33ef4dbb3a92a
Michał Potocki <s61451@student.ubb.edu.pl>
2025-04-24 19:27:06 +0200

    CollectionWindow działa

--- ViewModels/CollectionWindowViewModel.cs
+++ ViewModels/CollectionWindowViewModel.cs
@@ -3,12 +3,27 @@ using System;
 using System.Collections.Generic;
 using System.Reactive;
 using System.Text;
+using System.Threading.Tasks;
+using OmniMedia.Database; // Potrzebne do AppDatabase
+using Avalonia.Threading; // Potrzebne do Dispatcher.UIThread
+using System.Collections.ObjectModel; // Potrzebne do ObservableCollection
+using OmniMedia.Models; // Potrzebne do modelu Game
+using OmniMedia.ViewModels;
 
 namespace OmniMedia.ViewModels
 {
     // ViewModel dla okna "Przeglądaj swoją kolekcję"
     public class CollectionWindowViewModel : ViewModelBase
     {
+        // Właściwość, która przechowuje aktualnie wyświetlany ViewModel w dolnej części okna kolekcji
+        private ViewModelBase? _currentCollectionContent; // Może być null na początku
+        public ViewModelBase? CurrentCollectionContent
+        {
+            get => _currentCollectionContent;
+            set => this.RaiseAndSetIfChanged(ref _currentCollectionContent, value);
+        }
+
+        // Komendy dla przycisków w tym oknie
 
         // Komenda dla przycisku "Kolekcja Gier"
         public ReactiveCommand<Unit, Unit> OpenGameCollectionCommand { get; }
@@ -19,24 +34,53 @@ namespace OmniMedia.ViewModels
         // Komenda dla przycisku "Kolekcja Filmów"
         public ReactiveCommand<Unit, Unit> OpenMovieCollectionCommand { get; }
 
-        // TODO: Właściwość lub kolekcja dla miniatur ostatnio dodanych treści
-        // np. ObservableCollection<RecentlyAddedItemViewModel> RecentlyAddedItems { get; }
-        // Gdzie RecentlyAddedItemViewModel to ViewModel dla pojedynczej miniaturki
+        // TODO: Właściwość lub kolekcja dla miniatur ostatnio dodanych treści (jeśli chcesz je wyświetlać w tym ViewModelu)
+        // Na razie skupiamy się na przełączaniu widoków kolekcji
+        // public ObservableCollection<RecentlyAddedItemViewModel> RecentlyAddedItems { get; }
 
         // Konstruktor ViewModelu
         public CollectionWindowViewModel()
         {
             // Inicjalizacja komend przycisków
-            OpenGameCollectionCommand = ReactiveCommand.Create(() => { /* Logika otwierania widoku Kolekcji Gier */ });
-            OpenMusicCollectionCommand = ReactiveCommand.Create(() => { /* Logika otwierania widoku Kolekcji Muzyki */ });
-            OpenMovieCollectionCommand = ReactiveCommand.Create(() => { /* Logika otwierania widoku Kolekcji Filmów */ });
 
-            // TODO: Logika ładowania miniatur ostatnio dodanych treści
+            // Implementacja OpenGameCollectionCommand - ustawia CurrentCollectionContent na GameCollectionViewModel
+            OpenGameCollectionCommand = ReactiveCommand.Create(() =>
+            {
+                CurrentCollectionContent = new GameCollectionViewModel(); // Tworzymy i ustawiamy ViewModel dla Kolekcji Gier
+                System.Diagnostics.Debug.WriteLine("Kliknięto: Kolekcja Gier. Ustawiono GameCollectionViewModel."); // Komunikat debug
+            });
+
+            // Przykładowe inicjalizacje pozostałych komend (na razie puste)
+            OpenMusicCollectionCommand = ReactiveCommand.Create(() => {
+                // TODO: Logika otwierania widoku Kolekcji Muzyki
+                System.Diagnostics.Debug.WriteLine("Kliknięto: Kolekcja Muzyki");
+            });
+
+            OpenMovieCollectionCommand = ReactiveCommand.Create(() => {
+                // TODO: Logika otwierania widoku Kolekcji Filmów
+                System.Diagnostics.Debug.WriteLine("Kliknięto: Kolekcja Filmów");
+            });
+
+            // TODO: Logika ładowania miniatur ostatnio dodanych treści (jeśli będą w tym ViewModelu)
+            // Jeśli miniaturki mają być niezależne od wybranej kolekcji, ich ładowanie może być tutaj
         }
 
-        // Później dodać metody do interakcji z bazą danych w celu pobrania ostatnich elementów
-    }
+        // TODO: Możesz potrzebować ViewModelu dla pojedynczej miniaturki
+        // public class RecentlyAddedItemViewModel : ViewModelBase { ... }
 
-    // TODO: Możesz potrzebować ViewModelu dla pojedynczej miniaturki
-    // public class RecentlyAddedItemViewModel : ViewModelBase { ... }
+        // Metoda do ładowania gier (przeniesiona z GameCollectionViewModel do celów debugowania, można przenieść z powrotem)
+        // Możesz usunąć tę metodę LoadGamesAsync, jeśli już ją masz w GameCollectionViewModel.cs
+        /*
+        private async Task LoadGamesAsync()
+        {
+            var dummyGames = await App.Database.GetGamesAsync();
+
+            await Dispatcher.UIThread.InvokeAsync(() =>
+            {
+                // Tutaj docelowo zaktualizujesz kolekcję miniatur lub danych w tym ViewModelu
+                System.Diagnostics.Debug.WriteLine($"[CollectionWindowViewModel] Załadowano {dummyGames.Count} gier z bazy danych.");
+            });
+        }
+        */
+    }
 }
---
+++ ViewModels/GameCollectionViewModel.cs
@@ -0,0 +1,50 @@
+﻿using ReactiveUI;
+using System.Collections.ObjectModel;
+using OmniMedia.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using OmniMedia.Database;
+using Avalonia.Threading;
+using System.Diagnostics;
+
+namespace OmniMedia.ViewModels
+{
+    // ViewModel dla widoku Kolekcji Gier
+    public class GameCollectionViewModel : ViewModelBase
+    {
+        // Właściwość na kolekcję gier do wyświetlenia
+        public ObservableCollection<Game> Games { get; set; }
+
+        // Konstruktor ViewModelu
+        public GameCollectionViewModel()
+        {
+            Games = new ObservableCollection<Game>(); // Inicjalizacja kolekcji
+
+            // Wczytaj gry z bazy danych po utworzeniu ViewModelu
+            // Uruchamiamy ładowanie asynchronicznie, aby nie blokować UI
+            Task.Run(LoadGamesAsync);
+        }
+
+        // Metoda asynchroniczna do ładowania gier z bazy danych
+        private async Task LoadGamesAsync()
+        {
+            // Próba pobrania z bazy danych
+            var dummyGames = await App.Database.GetGamesAsync();
+
+            // Upewniamy się, że aktualizacja ObservableCollection odbywa się w wątku UI
+            await Dispatcher.UIThread.InvokeAsync(() =>
+            {
+                Games.Clear();
+                foreach (var game in dummyGames)
+                {
+                    Games.Add(game);
+                }
+                Debug.WriteLine($"[GameCollectionViewModel] Załadowano {Games.Count} gier z bazy danych.");
+            });
+        }
+
+        // Można dodać inne właściwości lub komendy, np. do sortowania, filtrowania, usuwania gier
+    }
+}
