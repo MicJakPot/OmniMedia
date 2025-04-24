c8e317fbef2c49c0603686904c9a01ac35abdde0
Michał Potocki <s61451@student.ubb.edu.pl>
2025-04-24 19:10:41 +0200

    Otwiera się okno kolekcji

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -3,6 +3,8 @@ using ReactiveUI;
 using System;
 using System.Reactive;
 using System.Threading.Tasks;
+using OmniMedia.Views; // Potrzebne do odwołania do CollectionWindow
+using System.Diagnostics; // Pozostawione na potrzeby innych komunikatów
 
 namespace OmniMedia.ViewModels
 {
@@ -34,67 +36,80 @@ namespace OmniMedia.ViewModels
         // Komenda dla przycisku "O Twórcach"
         public ReactiveCommand<Unit, Unit> OpenAboutCommand { get; }
 
+        // Właściwość do zarządzania wyświetlaną zawartością w głównym oknie (jeśli używana)
+        private ViewModelBase? _currentContent; // Używamy ViewModelBase? jeśli może być null
+        public ViewModelBase? CurrentContent
+        {
+            get => _currentContent;
+            set => this.RaiseAndSetIfChanged(ref _currentContent, value);
+        }
+
+
         public MainWindowViewModel()
         {
             // Inicjalizacja komend
-            // Na razie implementacja jest prosta - tylko wyświetlenie komunikatu w konsoli debugowania
-            // W przyszłości tutaj będzie logika nawigacji do odpowiednich widoków/ViewModeli
+            // Logika nawigacji do odpowiednich widoków/okien
 
-            OpenCollectionCommand = ReactiveCommand.CreateFromTask(async () =>
+            // Zmieniona implementacja OpenCollectionCommand - otwiera nowe okno CollectionWindow
+            OpenCollectionCommand = ReactiveCommand.Create(() => // Zmieniono na Create, bo Show() nie jest asynchroniczne
             {
-                // TODO: Implementuj logikę otwierania widoku kolekcji
-                System.Diagnostics.Debug.WriteLine("Kliknięto: Przeglądaj swoją Kolekcję");
-                await Task.CompletedTask; // Przykładowa asynchroniczność
+                // Tworzymy nową instancję CollectionWindow
+                var collectionWindow = new CollectionWindow
+                {
+                    // Ustawiamy DataContext nowego okna na nowy ViewModel CollectionWindowViewModel
+                    DataContext = new CollectionWindowViewModel()
+                };
+                // Wyświetlamy nowe okno
+                collectionWindow.Show();
+
+                System.Diagnostics.Debug.WriteLine("Kliknięto: Przeglądaj swoją Kolekcję. Otwarto CollectionWindow."); // Opcjonalny komunikat
             });
 
-            OpenGameSearchCommand = ReactiveCommand.CreateFromTask(async () =>
+            // Implementacja OpenGameSearchCommand - teraz ustawia CurrentContent w MainWindowViewModel
+            OpenGameSearchCommand = ReactiveCommand.Create(() =>
             {
-                // TODO: Implementuj logikę otwierania widoku wyszukiwania gier
-                System.Diagnostics.Debug.WriteLine("Kliknięto: Szukaj Gry");
-                await Task.CompletedTask;
+                // TODO: Zaimplementuj logikę otwierania widoku wyszukiwania gier w głównym oknie
+                // Ustawienie CurrentContent na GameSearchViewModel
+                CurrentContent = new GameSearchViewModel();
+                System.Diagnostics.Debug.WriteLine("Kliknięto: Szukaj Gry. Ustawiono GameSearchViewModel jako CurrentContent.");
             });
 
-            OpenMusicSearchCommand = ReactiveCommand.CreateFromTask(async () =>
-            {
-                // TODO: Implementuj logikę otwierania widoku wyszukiwania muzyki
+
+            // Przykładowe inicjalizacje pozostałych komend (możesz dostosować)
+            OpenMusicSearchCommand = ReactiveCommand.Create(() => {
+                // TODO: Logika otwierania widoku wyszukiwania muzyki
                 System.Diagnostics.Debug.WriteLine("Kliknięto: Szukaj Muzyki");
-                await Task.CompletedTask;
             });
 
-            OpenMovieSearchCommand = ReactiveCommand.CreateFromTask(async () =>
-            {
-                // TODO: Implementuj logikę otwierania widoku wyszukiwania filmów
+            OpenMovieSearchCommand = ReactiveCommand.Create(() => {
+                // TODO: Logika otwierania widoku wyszukiwania filmów
                 System.Diagnostics.Debug.WriteLine("Kliknięto: Szukaj Filmów");
-                await Task.CompletedTask;
             });
 
-            ExportDatabaseCommand = ReactiveCommand.CreateFromTask(async () =>
-            {
-                // TODO: Implementuj logikę eksportu bazy danych
+            ExportDatabaseCommand = ReactiveCommand.Create(() => {
+                // TODO: Logika eksportu bazy danych
                 System.Diagnostics.Debug.WriteLine("Kliknięto: Eksportuj Bazę");
-                await Task.CompletedTask;
             });
 
-            ImportDatabaseCommand = ReactiveCommand.CreateFromTask(async () =>
-            {
-                // TODO: Implementuj logikę importu bazy danych
+            ImportDatabaseCommand = ReactiveCommand.Create(() => {
+                // TODO: Logika importu bazy danych
                 System.Diagnostics.Debug.WriteLine("Kliknięto: Importuj Bazę");
-                await Task.CompletedTask;
             });
 
-            OpenSettingsCommand = ReactiveCommand.CreateFromTask(async () =>
-            {
-                // TODO: Implementuj logikę otwierania widoku ustawień
+            OpenSettingsCommand = ReactiveCommand.Create(() => {
+                // TODO: Logika otwierania widoku ustawień
                 System.Diagnostics.Debug.WriteLine("Kliknięto: Ustawienia");
-                await Task.CompletedTask;
             });
 
-            OpenAboutCommand = ReactiveCommand.CreateFromTask(async () =>
-            {
-                // TODO: Implementuj logikę otwierania widoku "O Twórcach"
+            OpenAboutCommand = ReactiveCommand.Create(() => {
+                // TODO: Logika otwierania widoku "O Twórcach"
                 System.Diagnostics.Debug.WriteLine("Kliknięto: O Twórcach");
-                await Task.CompletedTask;
             });
+
+
+            // Opcjonalnie: Ustaw początkową zawartość prawej części okna głównego przy starcie
+            // np. pusty ViewModel, widok powitalny, lub domyślny widok kolekcji (jeśli zdecydujesz się na wyświetlanie kolekcji w głównym oknie)
+            // CurrentContent = new SomeDefaultViewModel();
         }
     }
 }
