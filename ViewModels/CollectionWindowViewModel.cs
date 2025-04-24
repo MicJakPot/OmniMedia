fc14109a8fed8dd3e3b75f4b889d6843bff74bfc
Michał Potocki <s61451@student.ubb.edu.pl>
2025-04-24 18:58:11 +0200

    Utworzony ViewModel dla okna Kolekcji (CollectionWindowViewModel)

---
+++ ViewModels/CollectionWindowViewModel.cs
@@ -0,0 +1,42 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Reactive;
+using System.Text;
+
+namespace OmniMedia.ViewModels
+{
+    // ViewModel dla okna "Przeglądaj swoją kolekcję"
+    public class CollectionWindowViewModel : ViewModelBase
+    {
+
+        // Komenda dla przycisku "Kolekcja Gier"
+        public ReactiveCommand<Unit, Unit> OpenGameCollectionCommand { get; }
+
+        // Komenda dla przycisku "Kolekcja Muzyki"
+        public ReactiveCommand<Unit, Unit> OpenMusicCollectionCommand { get; }
+
+        // Komenda dla przycisku "Kolekcja Filmów"
+        public ReactiveCommand<Unit, Unit> OpenMovieCollectionCommand { get; }
+
+        // TODO: Właściwość lub kolekcja dla miniatur ostatnio dodanych treści
+        // np. ObservableCollection<RecentlyAddedItemViewModel> RecentlyAddedItems { get; }
+        // Gdzie RecentlyAddedItemViewModel to ViewModel dla pojedynczej miniaturki
+
+        // Konstruktor ViewModelu
+        public CollectionWindowViewModel()
+        {
+            // Inicjalizacja komend przycisków
+            OpenGameCollectionCommand = ReactiveCommand.Create(() => { /* Logika otwierania widoku Kolekcji Gier */ });
+            OpenMusicCollectionCommand = ReactiveCommand.Create(() => { /* Logika otwierania widoku Kolekcji Muzyki */ });
+            OpenMovieCollectionCommand = ReactiveCommand.Create(() => { /* Logika otwierania widoku Kolekcji Filmów */ });
+
+            // TODO: Logika ładowania miniatur ostatnio dodanych treści
+        }
+
+        // Później dodać metody do interakcji z bazą danych w celu pobrania ostatnich elementów
+    }
+
+    // TODO: Możesz potrzebować ViewModelu dla pojedynczej miniaturki
+    // public class RecentlyAddedItemViewModel : ViewModelBase { ... }
+}
